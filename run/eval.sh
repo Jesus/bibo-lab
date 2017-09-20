46727b0e9d1a1eff16facdeb088fdfd7f78bad92
Jesús Burgos <jesus@visrez.com>
2017-09-21 00:06:56 +0200

    Fix logging

--- run/eval.sh
+++ run/eval.sh
@@ -4,4 +4,4 @@ python3 "$OBJECT_DETECTION_PATH/eval.py" \
   --logtostderr \
   --pipeline_config_path=config/bib_detector.config \
   --checkpoint_dir=model/train --eval_dir=model/eval \
-  | tee -a "log/eval.log" 2>&1
+  2>&1 | tee -a "log/eval.log"
--- run/train.sh
+++ run/train.sh
@@ -4,4 +4,4 @@ python3 "$OBJECT_DETECTION_PATH/train.py" \
   --logtostderr \
   --pipeline_config_path=config/bib_detector.config \
   --train_dir=model/train \
-  | tee -a "log/train.log" 2>&1
+  2>&1 | tee -a "log/train.log"
