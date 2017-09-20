b41b79e092ffc5a6d85c7699ebe154cca5b36f4e
Jesús Burgos <jesus@visrez.com>
2017-09-21 00:01:18 +0200

    Adds some logging support

--- run/eval.sh
+++ run/eval.sh
@@ -3,4 +3,5 @@ source run/config.sh
 python3 "$OBJECT_DETECTION_PATH/eval.py" \
   --logtostderr \
   --pipeline_config_path=config/bib_detector.config \
-  --checkpoint_dir=model/train --eval_dir=model/eval
+  --checkpoint_dir=model/train --eval_dir=model/eval \
+  | tee -a "log/eval.log" 2>&1
--- run/train.sh
+++ run/train.sh
@@ -3,4 +3,5 @@ source run/config.sh
 python3 "$OBJECT_DETECTION_PATH/train.py" \
   --logtostderr \
   --pipeline_config_path=config/bib_detector.config \
-  --train_dir=model/train
+  --train_dir=model/train \
+  | tee -a "log/train.log" 2>&1
