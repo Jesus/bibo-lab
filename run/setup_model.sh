6ea1615f683eb2898d6b52b040e4cf55ed86bddd
Jesús Burgos <jesus@visrez.com>
2017-10-10 18:58:55 +0200

    Replaces DNN
    Now using `faster_rcnn_inception_resnet_v2_atrous_coco`.

--- run/setup_model.sh
+++ run/setup_model.sh
@@ -2,7 +2,7 @@ source run/config.sh
 
 # You can find all available pre-trained models in the `tensorflow/models` repo,
 # in `object_detection/g3doc/detection_model_zoo.md`.
-MODEL_URL="http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_11_06_2017.tar.gz"
+MODEL_URL="http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_resnet_v2_atrous_coco_11_06_2017.tar.gz"
 MODEL_PATH="tmp/pre-trained-model.tar.gz"
 
 if [ ! -f "$MODEL_PATH" ]; then
