ec7cbfd2aee4ddd46020d56f3b6eb345ca67bcd5
Jesús Burgos <jesus@visrez.com>
2017-10-27 17:34:53 +0200

    A working detector!

--- detect.py
+++ detect.py
@@ -11,52 +11,51 @@ PATH_TO_LABELS = 'model/label_map.pbtxt'
 input_path = "input.jpg"
 output_path = "output.jpg"
 
-class ObjectDetector(object):
-    def __init__(self):
-        self.detection_graph = self._build_graph()
-        self.sess = tf.Session(graph=self.detection_graph)
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
 
-        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=90, use_display_name=True)
-        self.category_index = label_map_util.create_category_index(categories)
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=1, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
 
-    def _build_graph(self):
-        detection_graph = tf.Graph()
-        with detection_graph.as_default():
-            od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-            serialized_graph = fid.read()
-            od_graph_def.ParseFromString(serialized_graph)
-            tf.import_graph_def(od_graph_def, name='')
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
 
-        return detection_graph
+with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+        # Definite input and output Tensors for detection_graph
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
-    def _load_image_into_numpy_array(self, image):
-        (im_width, im_height) = image.size
-        return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+        # Each box represents a part of the image where a particular object was detected.
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
-    def detect(self, image):
-        image_np = self._load_image_into_numpy_array(image)
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+        # the array based representation of the image will be used later in order to prepare the
+        # result image with boxes and labels on it.
+        image = Image.open(input_path)
+        image_np = load_image_into_numpy_array(image)
+
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
         image_np_expanded = np.expand_dims(image_np, axis=0)
+        (boxes, scores, classes, num) = sess.run([detection_boxes, detection_scores, detection_classes, num_detections], feed_dict={image_tensor: image_np_expanded})
 
-        image_tensor = graph.get_tensor_by_name('image_tensor:0')
-        boxes = graph.get_tensor_by_name('detection_boxes:0')
-        scores = graph.get_tensor_by_name('detection_scores:0')
-        classes = graph.get_tensor_by_name('detection_classes:0')
-        num_detections = graph.get_tensor_by_name('num_detections:0')
+        boxes, scores, classes, num = map(np.squeeze, [boxes, scores, classes, num])
 
-        (boxes, scores, classes, num_detections) = self.sess.run([boxes, scores, classes, num_detections], feed_dict={image_tensor: image_np_expanded})
+        for i in range(int(num)):
+            if scores[i] < 0.7: continue
 
-        boxes, scores, classes, num_detections = map(np.squeeze, [boxes, scores, classes, num_detections])
-
-        return boxes, scores, classes.astype(int), num_detections
-
-detector = ObjectDetector()
-image = Image.open(input_path).convert('RGB')
-detector.detect(image)
-# continue here:
-# https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
-# 
-# boxes, scores, classes, num_detections =
-# for i in range(num_detections):
-#     print(boxes[i])
+            print("")
+            print(f"box: {boxes[i]}")
+            print(f"score: {scores[i]}")
+            print(f"class: {category_index[classes[i]]['name']}")
