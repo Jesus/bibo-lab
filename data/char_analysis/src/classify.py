de71ab4074765210355224339b4e963e4a1fac1e
Jesús Burgos <jesus@visrez.com>
2017-11-25 16:49:08 +0100

    Updates classifier script to allow going back

--- data/char_analysis/src/classify.py
+++ data/char_analysis/src/classify.py
@@ -10,19 +10,21 @@ import os
 bibs_path      = "data/char_analysis/bibs"
 chars_path     = "data/char_analysis/chars"
 
-annotations    = "data/char_analysis/annotations.csv"
+annotations_path = "data/char_analysis/annotations.csv"
+annotations = {}
 
-annotated_files = []
-with open(annotations, "r") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ",")
-    for annotation in csv_reader:
-        annotated_files.append(annotation[0])
+def save_annotations():
+    print("-" * 80)
+    for image_path, digit in annotations.items():
+        print(f"{image_path}: {digit}")
 
-for image_path in glob(os.path.join(chars_path, "**/*.*.jpg"), recursive=True):
-    if image_path in annotated_files:
-        print(f"Already annotated '{image_path}'")
-        continue
+        with open(annotations_path, 'a') as csv_file:
+            if digit == None:
+                csv_file.write(f"{image_path},n,\n")
+            else:
+                csv_file.write(f"{image_path},y,{digit}\n")
 
+def annotate(image_path):
     print(image_path)
     file_name = os.path.basename(image_path)
     bib_file_name, coords, bib_file_extension = file_name.split(".")
@@ -49,19 +51,49 @@ for image_path in glob(os.path.join(chars_path, "**/*.*.jpg"), recursive=True):
 
     cv2.imshow("Digit", digit)
     cv2.imshow("Bib", bib)
-    key = cv2.waitKey(0)
 
-    if key == 46 or key == 43 or key == 45: # chars '.', '+' & '-'
-        digit = None
-    elif key >= 48 and key <= 57: # digit
-        digit = key - 48
-    else:
-        exit(1)
+    while True:
+        key = cv2.waitKey(0)
+        if key == 46 or key == 43 or key == 45 or key == 97: # chars '.', '+' & '-'
+            digit = None
+            break
+        elif key >= 48 and key <= 57: # digit
+            digit = key - 48
+            break
+        elif key == 8:
+            return False
+        elif key == 113:
+            save_annotations()
+            exit(0)
+        else:
+            print(f"Invalid key: {key}")
 
     print(f"Digit: {digit}")
+    annotations[image_path] = digit
 
-    with open(annotations, 'a') as csv_file:
-        if digit == None:
-            csv_file.write(f"{image_path},n,\n")
-        else:
-            csv_file.write(f"{image_path},y,{digit}\n")
+    return True
+
+annotated_files = []
+with open(annotations_path, "r") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ",")
+    for annotation in csv_reader:
+        annotated_files.append(annotation[0])
+
+image_paths = glob(os.path.join(chars_path, "**/*.*.jpg"), recursive=True)
+index = 0
+count = 0
+while index < len(image_paths) and count < 500:
+    image_path = image_paths[index]
+    if image_path in annotated_files:
+        print(f"Already annotated '{image_path}'")
+        index = index + 1
+        continue
+
+    if annotate(image_path):
+        index = index + 1
+        count = count + 1
+    else:
+        index = index - 1
+        count = count - 1
+
+save_annotations()
