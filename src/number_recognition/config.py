d4d6b57fb0d9c3421d490dd4b3ca97471a547e3b
Jesús Burgos <jesus@visrez.com>
2018-12-29 10:55:21 +0100

    Updates config to export new frozen graph

--- src/number_recognition/config.py
+++ src/number_recognition/config.py
@@ -6,8 +6,8 @@ annotations_path = "data/number_recognition/annotations/bibs.csv"
 model_path       = "models/attention_ocr/python/datasets/data/bibo"
 train_path       = "models/attention_ocr/python/workdir/train"
 
-checkpoint = os.path.join(train_path, "model.ckpt-90482")
+checkpoint = os.path.join(train_path, "model.ckpt-93215")
 batch_size = 32
-width, height = 80, 80
+width, height = 120, 120
 
 frozen_inference_graph_path = "number_recognition_frozen_inference_graph.pb"
